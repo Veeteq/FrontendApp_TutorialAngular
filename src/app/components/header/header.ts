d32a8d27e95d46c2cca7c9c684c41685cf51d487
Witold Wojnarowicz <witold.wojnarowicz@gmail.com>
2025-11-03 15:35:32 +0100

    CR-002: Create reusable button

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
+import { PrimaryButton } from '../reusable/primary-button/primary-button';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [PrimaryButton],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
-title = signal('My E-Commerce App');
+  title = signal('My E-Commerce App');
 
+  onBtnClicked() {
+    console.log('Header button clicked');
+  }
 }

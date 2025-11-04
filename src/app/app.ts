6cbb44b068fc3c857bb81bec72edc988296dcd14
Witold Wojnarowicz <witold.wojnarowicz@gmail.com>
2025-11-04 10:26:42 +0100

    BUG-001: Switch from .scss to .css

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
 import { Component, signal } from '@angular/core';
 import { Header } from "./components/header/header";
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [ Header],
+  imports: [ Header, ReactiveFormsModule ],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('TutorialECom');
+  myForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      date: [new Date().toISOString().substring(0, 10)] // Format to YYYY-MM-DD
+    });
+  }
+
+  onSubmit() {
+    console.log(this.myForm.value);
+  }
 }
